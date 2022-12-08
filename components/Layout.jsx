2f8a50fcdd79fb8b1f7ab30ac5511d411fab5e63
Randy Castillo <randycastillo.ap@gmail.com>
2022-12-07 23:12:55 -0500

    fixing-next-hosting-error

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,8 +4,8 @@ import Header from './Header';
 const Layout = ({ children }) => {
   return (
     <div>
-      {/* <Header />
-      {children} */}
+      <Header />
+      {/* {children} */}
     </div>
   )
 }
