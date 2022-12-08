33459cfc4d48946e4d021ea448732efbdb386a9b
Randy Castillo <randycastillo.ap@gmail.com>
2022-12-07 23:16:16 -0500

    fixing-next-hosting-error

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,18 @@ export default function Home ({ posts }) {
   return (
     <div className="container mx-auto px-6 mb-4">
       <FeaturedPosts />
-      {/* <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-4 col-span-1">
           <PostWidget className="lg:sticky relative top-8" posts={posts.slice(0, 5)}/>
         </div>
-        <div className="lg:col-span-8 col-span-1">
+        {/* <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
             <div key={index}>
               <PostCard key={index} post={post} />
             </div>
           ))}
-        </div>
-      </div> */}
+        </div> */}
+      </div>
     </div>
   );
 }
